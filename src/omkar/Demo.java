d920cac1634868834cd2e89d719bb3743aeab507
Quickyes <maheshgadade151@gmail.com>
2022-03-21 04:05:01 -0700

    add packages

---
+++ src/omkar/Demo.java
@@ -0,0 +1,7 @@
+package omkar;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/rutuja/Demo.java
@@ -0,0 +1,7 @@
+package rutuja;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
