ef39acc3b1d9c408c3d858d30ec81cf8b1bb819b
omkar <omkargurav7792@gmail.com>
2022-03-22 17:56:44 +0530

    addition of two num

---
+++ src/omkar/Calculation.java
@@ -0,0 +1,14 @@
+package omkar;
+
+import java.util.Scanner;
+
+public class Calculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextByte();
+        int c=a+b;
+        System.out.println("addition of two number="+c);
+
+    }
+}
