58b2f8e2315c0b3c3b8367142a568ff9109166a8
hp <akashsalvi8676@gmail.com>
2022-03-25 16:15:16 +0530

    No ParamConstructor,WithoutParameter,ParamConstructor

--- src/akash/constructor/NoParamConstructor.java
+++ src/akash/constructor/NoParamConstructor.java
@@ -3,20 +3,23 @@ package akash.constructor;
 public class NoParamConstructor {
     int a;
     float b;
-    // no args constructor
+    //No parameter/args Constructor
     public NoParamConstructor(){
         a=20;
-        b=30;
+        b=34;
         System.out.println("a is ="+a);
         System.out.println("b is ="+b);
     }
-    public void NoParamConstructor(){
-        System.out.println("I am in NoParamConstructor() method");
+    public void NoParameterConstructor(){
+        System.out.println("I am in NoParameterConstructor() method");
 
     }
 
     public static void main(String[] args) {
-        NoParamConstructor obj= new NoParamConstructor();
-        obj.NoParamConstructor();
+        NoParamConstructor obj = new NoParamConstructor();
+        obj.NoParameterConstructor();
     }
+
+
+
 }
