632a42ddaab0895dee692cab73d2f75cdfcbad4d
Admin <prasadnarsale46209@gmail.com>
2022-03-26 23:36:35 +0530

    Inheritance

---
+++ src/prasad/inheritance/Super.java
@@ -0,0 +1,10 @@
+package prasad.inheritance;
+
+public class Super {
+    public void Test1(){
+        System.out.println("I am in Test1() Method in super class ");
+    }
+    public void Test2() {
+        System.out.println("I am in Test2() method in super class ");
+    }
+}
