f7bc62a89dd8d2a0fe2f926f23e48250f0e163fb
hp <akashsalvi8676@gmail.com>
2022-03-28 17:21:17 +0530

    aLL

---
+++ src/akash/multilevelInheritance/Akash1.java
@@ -0,0 +1,11 @@
+package akash.multilevelInheritance;
+
+public class Akash1 {
+
+        public void test1(){
+            System.out.println("I am in test1 Method");
+        }
+        public void test2(){
+            System.out.println("I am in test2 Method");
+        }
+}
